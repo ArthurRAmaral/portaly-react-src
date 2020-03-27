e57fe657b3a501bdbdce9847c8638ed6e8bcdcbb
Guilherme <guilhergoa@hotmail.com>
2020-03-27 19:32:09 -0300

    carrinho fix name

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Routes from "./routes";
 import Footer from "./components/Footer";
-import funcoesCarrinho from "./util/Carrinho";
+import Carrinho from "./util/Carrinho";
 import "materialize-css/dist/css/materialize.min.css";
 import "./css/Global.css";
 
 
 const App = () => {
 
-  funcoesCarrinho.setCarrinho();
+  Carrinho.setCarrinho();
 
   return (
     <BrowserRouter>
