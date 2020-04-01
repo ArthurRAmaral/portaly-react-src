86224b223e1f1e27482d93e3f6f5d3e48815da10
Guilherme <guilhergoa@hotmail.com>
2020-04-01 20:24:43 -0300

    Base do refatoramento

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Routes from "./routes";
 import Footer from "./components/Footer";
+import LineLoading from "./components/loading/LineLoading"
 import Carrinho from "./util/Carrinho";
-import "materialize-css/dist/css/materialize.min.css";
+import ApiWooCommerce from "./util/ApiWooCommerce"
 import "./css/Global.css";
+import "materialize-css/dist/css/materialize.min.css";
 
 
-const App = () => {
 
-   Carrinho.setCarrinho();
 
-   return (
-      <BrowserRouter>
-         <Header />
-         <Routes />
-         <Footer />
-      </BrowserRouter>
-   );
+class App extends Component {
+
+   
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         produtos: []
+      }
+   }
+
+
+   componentDidMount() {
+      ApiWooCommerce.getAll().then(res => {
+         this.setState({ produtos: [...res.data] });
+      });
+   }
+
+
+   render() {
+
+      Carrinho.setCarrinho();
+
+      return (
+         <BrowserRouter>
+               <Header categorias={this.state.produtos} />
+               {/* <Routes /> */}
+               {/* <Footer /> */}
+            </BrowserRouter>
+      )
+   }
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,20 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import TopHeader from "./TopHeader";
 import MainHeader from "./MainHeader"
 import "../../css/Header.css";
 
-class Headers extends Component {
+const Headers = (produtos) => {
+   
+   console.log(produtos);
 
-  render() {
     return (
       <Fragment>
-        <header className="header">
+        <header  className="header">
           <TopHeader />
-          <MainHeader />
+          {/* <MainHeader /> */}
         </header>
       </Fragment>
     );
-  }
 }
 
 export default Headers;
