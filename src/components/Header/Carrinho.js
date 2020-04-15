1510a603ef1b90852fc3a824a6e60e1b470d7cc2
Matheus <matheusfelipeferreiramartins@gmail.com>
2020-04-15 15:45:21 -0300

    Melhorando aparência do valor do carrinho

--- src/components/Header/Carrinho.js
+++ src/components/Header/Carrinho.js
@@ -5,7 +5,9 @@ import { NavLink } from "react-router-dom";
 import InitPath from "../../services/InitPath";
 
 const CarrinhoCompras = () => {
-   const valor = Carrinho.getValorCarrinho();
+   const valor = (Math.round(Carrinho.getValorCarrinho() * 100) / 100).toFixed(
+      2
+   );
 
    return (
       <NavLink key={`carrinho`} to={`${InitPath}/meuCarrinho`}>
