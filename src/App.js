3e66f4046e32c8e6d7b15627cea851e9e4877b53
Guilherme <guilhergoa@hotmail.com>
2020-04-15 15:43:08 -0300

    Configuração para padronizar o codigo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Routes from "./routes";
-import Footer from "./components/Footer";
-import Carrinho from "./util/Carrinho";
-import "materialize-css/dist/css/materialize.min.css";
-import "./css/Global.css";
-
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Routes from './routes';
+import Footer from './components/Footer';
+import Carrinho from './util/Carrinho';
+import 'materialize-css/dist/css/materialize.min.css';
+import './css/Global.css';
 
 const App = () => {
+  Carrinho.setCarrinho();
 
-   Carrinho.setCarrinho();
-
-   return (
+  return (
       <BrowserRouter>
          <Header />
          <Routes />
          <Footer />
       </BrowserRouter>
-   );
+  );
 };
 
 export default App;
--- src/pages/PaginaCategoria.js
+++ src/pages/PaginaCategoria.js
@@ -1,48 +1,48 @@
-import React, { Component, Fragment } from "react";
-import ApiWooCommerce from "../util/ApiWooCommerce";
-import MostraProdutos from "../components/MostraProdutos";
-import LineLoaging from "../components/loading/LineLoading";
-import Carrinho from "../util/Carrinho";
+import React, { Component, Fragment } from 'react';
+import ApiWooCommerce from '../util/ApiWooCommerce';
+import MostraProdutos from '../components/MostraProdutos';
+import LineLoaging from '../components/loading/LineLoading';
+import Carrinho from '../util/Carrinho';
 
 class PaginaCategorias extends Component {
-   constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-         produtos: [],
-         paginaId: props.match.params.id
-      };
-   }
+    this.state = {
+      produtos: [],
+      paginaId: props.match.params.id,
+    };
+  }
 
-   componentDidMount() {
-      this.chamaApiParaRceberProdutos(this.state.paginaId);
-   }
+  componentDidMount() {
+    this.chamaApiParaRceberProdutos(this.state.paginaId);
+  }
 
-   componentWillReceiveProps(nextProps) {
-      const id = nextProps.match.params.id;
-      this.chamaApiParaRceberProdutos(id);
-   }
+  componentWillReceiveProps(nextProps) {
+    const { id } = nextProps.match.params;
+    this.chamaApiParaRceberProdutos(id);
+  }
 
-   chamaApiParaRceberProdutos(id) {
-      ApiWooCommerce.getCategoria(id)
-         .then(res => {
-            this.setState({ produtos: [...res.data], paginaId: id });
-         })
-         .catch(console.log("Carregando"));
-   }
+  chamaApiParaRceberProdutos(id) {
+    ApiWooCommerce.getCategoria(id)
+      .then((res) => {
+        this.setState({ produtos: [...res.data], paginaId: id });
+      })
+      .catch(console.log('Carregando'));
+  }
 
-   render() {
-      Carrinho.setCarrinho();
+  render() {
+    Carrinho.setCarrinho();
 
-      return (
+    return (
          <Fragment>
-            {this.state.produtos.length > 0 &&
-            this.state.paginaId === this.props.match.params.id
-               ? MostraProdutos(this.state.produtos)
-               : LineLoaging()}
+            {this.state.produtos.length > 0
+            && this.state.paginaId === this.props.match.params.id
+              ? MostraProdutos(this.state.produtos)
+              : LineLoaging()}
          </Fragment>
-      );
-   }
+    );
+  }
 }
 
 export default PaginaCategorias;
