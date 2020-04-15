2d84d8d73ee5064ccd829fd2724d6f28dcab58f7
Guilherme <guilhergoa@hotmail.com>
2020-04-15 15:49:10 -0300

    Adiciona material-ui em package.json

--- src/pages/ProdutoBuscado.js
+++ src/pages/ProdutoBuscado.js
@@ -1,45 +1,43 @@
-import React, { Component, Fragment } from "react";
-import ApiWooCommerce from "../util/ApiWooCommerce";
-import LineLoading from "../components/loading/LineLoading";
-import MostrarProdutos from "../components/MostraProdutos";
-import Carrinho from "../util/Carrinho";
+import React, { Component, Fragment } from 'react';
+import ApiWooCommerce from '../util/ApiWooCommerce';
+import LineLoading from '../components/loading/LineLoading';
+import MostrarProdutos from '../components/MostraProdutos';
+import Carrinho from '../util/Carrinho';
 
 class ProdutosBuscado extends Component {
-   constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-         produtos: null,
-      };
-   }
+    this.state = {
+      produtos: null,
+    };
+  }
 
-   componentDidMount() {
-      const value = this.props.match.params.value;
+  componentDidMount() {
+    const { value } = this.props.match.params;
 
-      ApiWooCommerce.getAll().then((res) => {
-         let produtos = res.data.filter((produto) =>
-            produto.name.toString().toUpperCase().includes(value.toUpperCase())
-         );
+    ApiWooCommerce.getAll().then((res) => {
+      const produtos = res.data.filter((produto) => produto.name.toString().toUpperCase().includes(value.toUpperCase()));
 
-         this.setState({
-            produtos: [...produtos],
-         });
+      this.setState({
+        produtos: [...produtos],
       });
-   }
+    });
+  }
 
-   render() {
-      Carrinho.setCarrinho();
+  render() {
+    Carrinho.setCarrinho();
 
-      return (
+    return (
          <Fragment>
             {this.state.produtos !== null ? (
-               MostrarProdutos(this.state.produtos)
+              MostrarProdutos(this.state.produtos)
             ) : (
                <LineLoading />
             )}
          </Fragment>
-      );
-   }
+    );
+  }
 }
 
 export default ProdutosBuscado;
