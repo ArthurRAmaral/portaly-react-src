21f57b24680a790f1cb0df4bef5fe12fe1a8333f
Guilherme <guilhergoa@hotmail.com>
2020-05-04 22:43:42 -0300

    mudanças no css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import React, { Component, Fragment } from "react";
 //From components
 import TopHeader from "./TopHeader/TopHeader";
 import MainHeader from "./MainHeader/MainHeader";
-import "../../css/Header.css";
 
 class Headers extends Component {
   render() {
