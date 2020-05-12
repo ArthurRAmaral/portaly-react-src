ae4ad9df8e94a1bfda232b7442c28520800cb202
Guilherme <guilhergoa@hotmail.com>
2020-05-12 14:58:54 -0300

    add redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,14 @@ import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import { createPromise } from "redux-promise-middleware";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // import the root reducer
 import rootReducer from "./reducres/index";
 
 const middleware = applyMiddleware(createPromise(), thunk, createLogger());
 
-const store = createStore(rootReducer, middleware);
+const store = createStore(rootReducer, composeWithDevTools(middleware));
 
 // store.subscribe(() => console.log("algo mudou", store.geState()));
 
