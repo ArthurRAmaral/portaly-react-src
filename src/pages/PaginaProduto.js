cc93bf1885c8a903537f188d49aafa0c6f506ac5
Guilherme <guilhergoa@hotmail.com>
2020-05-13 23:41:45 -0300

    produto carregado do redux

--- src/pages/PaginaProduto.js
+++ src/pages/PaginaProduto.js
@@ -1,21 +1,16 @@
 //From depedencies
 import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 
 //From components
 import LineLoading from "../components/loading/LineLoading";
 import VerticalTab from "../components/ImagensTab/VerticalTab";
 import InformacoesProduto from "../components/informacoesProduto/informacoesProduto";
 
-//From services
-import ApiProdutos from "../services/ApiProdutos";
-
-//From util
-import Carrinho from "../util/Carrinho";
-
 //From Material-ui
 import Grid from "@material-ui/core/Grid";
 
-export default class PaginaProduto extends Component {
+class PaginaProduto extends Component {
   constructor(props) {
     super(props);
 
@@ -26,20 +21,17 @@ export default class PaginaProduto extends Component {
 
   async componentDidMount() {
     const { slug } = this.props.match.params;
+    let produto;
 
-    await ApiProdutos.getProductBySlug(slug).then((res) =>
-      this.setState({ produto: res.data[0] })
-    );
+    Object.values(this.props.produtos).forEach((prods) => {
+      prods.forEach((prod) => {
+        if (prod.slug == slug) {
+          produto = prod;
+        }
+      });
+    });
+    this.setState({ produto: produto });
   }
-
-  handleSubmit = (e) => {
-    const { produto } = this.state;
-
-    Carrinho.addItem(produto.id);
-
-    // this.props.history.push("/");
-    window.location.reload();
-  };
 
   renderProduto = () => {
     const { produto } = this.state;
@@ -72,34 +64,6 @@ export default class PaginaProduto extends Component {
   }
 }
 
-// <div className="produtomostrado s12 m6 card-large">
-// <div className="card">
-//   <div className="card-image">
-//     <img
-//       className="principal"
-//       src={
-//         produto.images.length > 0 ? produto.images[0].src : imgDefault
-//       }
-//       alt=""
-//     />
-//     <Link
-//       to="#"
-//       className="btn-floating halfway-fab waves-effect waves-light red"
-//     >
-//       <i onClick={this.handleSubmit} className="material-icons">
-//         add
-//       </i>
-//     </Link>
-//   </div>
-//   <div className="card-content">
-//     <p className="nome grey-text text-darken-4">{produto.name}</p>
-//     <p className="preco">R$: {produto.price}</p>
-//     {produto.short_description !== ""
-//       ? produto.short_description.substring(
-//           3,
-//           produto.short_description.length - 5
-//         )
-//       : ""}
-//   </div>
-// </div>
-// </div>
+const mapStateToProps = (state) => ({ produtos: state.produtos });
+
+export default connect(mapStateToProps, null)(PaginaProduto);
